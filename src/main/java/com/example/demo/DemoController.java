a9f946fd621d3cb0700e0b2276128bd5cde56fae
Subhra-M <65274589+Subhra-M@users.noreply.github.com>
2020-11-05 16:23:24 +0530

    Update DemoController.java

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,10 +1,8 @@
 package com.example.demo;
 
-import com.example.demo.Repository.UserRepository;
-import com.example.demo.model.BasicUser;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
@@ -19,10 +17,4 @@ public class DemoController {
         return "Hello world Azure";
     }
 
-    @GetMapping("/id/{id}")
-    public String userInfo(@PathVariable("id") Long id){
-        Optional<BasicUser> basicUser = userRepository.findById(id);
-        return basicUser.map(user -> "Hello " + user.getName() + " to new world of possibilities")
-                .orElse("Hello guest to new world of possibilities");
-    }
 }
